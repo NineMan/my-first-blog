53df3cff8bcdb2e44533c0380e467ce53a688b32
NineMan <nelubinmv@inbox.ru>
2020-08-30 00:18:05 +0400

    Add login and logout

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
